2ef56fcc977bf78fc8dac5291345e4d3c4b30b8a
MissFilly <lady.filly@gmail.com>
2012-12-08 16:23:58 -0300

    Generation of Android resources files

--- app/views.py
+++ app/views.py
@@ -6,17 +6,19 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from app.forms import RegistrationForm, LoginForm, GenerateForm
-from app.models import Translator, String, Language
+from app.models import Translator, String
 from django.contrib.auth import authenticate, login, logout
 from datetime import datetime
 from localization.utils import extra_funcs, generate
 
 CURRENT_VALUES = {}
 
+
 def MainPageHandler(request):
     user = request.user
     return render_to_response('index.html', {'user': user},
                               context_instance=RequestContext(request))
+
 
 def TranslatorRegistration(request):
     if request.user.is_authenticated():
@@ -25,15 +27,15 @@ def TranslatorRegistration(request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
             user = User.objects.create_user(
-                                       username = form.cleaned_data['username'],
-                                       email = form.cleaned_data['email'],
-                                       password = form.cleaned_data['password'])
+                                       username=form.cleaned_data['username'],
+                                       email=form.cleaned_data['email'],
+                                       password=form.cleaned_data['password'])
             user.first_name = form.cleaned_data['first_name']
             user.last_name = form.cleaned_data['last_name']
             user.save()
-            translator = Translator(user = user,
-                                    language = form.cleaned_data['language'],
-                                    words_translated = 0)
+            translator = Translator(user=user,
+                                    language=form.cleaned_data['language'],
+                                    words_translated=0)
             translator.save()
             return HttpResponseRedirect('/profile/')
         else:
@@ -45,6 +47,7 @@ def TranslatorRegistration(request):
         context = {'form': form}
         return render_to_response('register.html', context,
                                   context_instance=RequestContext(request))
+
 
 def LoginRequest(request):
     if request.user.is_authenticated():
@@ -67,13 +70,15 @@ def LoginRequest(request):
     else:
         ''' User is not submitting the form, show the login form '''
         form = LoginForm()
-        context = {'form': form, 'user': request.user }
+        context = {'form': form, 'user': request.user}
         return render_to_response('login.html', context,
                                   context_instance=RequestContext(request))
+
 
 def LogoutRequest(request):
     logout(request)
     return HttpResponseRedirect('/')
+
 
 @login_required
 def ProfileHandler(request):
@@ -92,18 +97,18 @@ def TranslationHandler(request):
         strings_list = extra_funcs.strings_to_translate(request)
     except Translator.DoesNotExist:
         return HttpResponseRedirect('/login/')
-    if request.method=='POST':
+    if request.method == 'POST':
         keys = request.POST.keys()
         keys.remove('csrfmiddlewaretoken')
         words_count = 0
 
         for key in keys:
-            if request.POST.get(key): # Translation field in form is not empty
+            if request.POST.get(key):  # Translation field in form is not empty
                 original = String.objects.get(id=int(key))
                 # The string already has a translation, it must be updated
                 try:
                     string = String.objects.get(language=translator.language,
-                 		                        original_string=original)
+                                                original_string=original)
                     if string in strings_list:
                         string.text = request.POST.get(key)
                         string.last_modif = datetime.now()
@@ -116,11 +121,11 @@ def TranslationHandler(request):
 
                 # The translated string must be created
                 except String.DoesNotExist:
-                    print original
-                    string = String(language = translator.language,
-                                    translator = request.user.get_profile(),
-                                    text = request.POST.get(key),
-                                    original_string = original)
+                    print(original)
+                    string = String(language=translator.language,
+                                    translator=request.user.get_profile(),
+                                    text=request.POST.get(key),
+                                    original_string=original)
                     string.save()
                 words_count += len(original.text.split())
         translator.words_translated += words_count
@@ -128,7 +133,7 @@ def TranslationHandler(request):
         return HttpResponseRedirect('/translate/')
 
     else:
-        paginator = Paginator(strings_list, 2) # Show 15 strings per page
+        paginator = Paginator(strings_list, 2)  # Show 15 strings per page
         page = request.GET.get('page')
         try:
             strings = paginator.page(page)
@@ -141,20 +146,25 @@ def TranslationHandler(request):
         return render_to_response('translate.html', context,
                                   context_instance=RequestContext(request))
 
+
 @login_required
 def ModifyStringsHandler(request):
     translator = request.user.get_profile
     if request.method == 'POST':
-        modified_translations = []
         keys = request.POST.keys()
         keys.remove('csrfmiddlewaretoken')
         global CURRENT_VALUES
         for key in keys:
-            if request.POST.get(key) and request.POST.get(key) != CURRENT_VALUES[int(key)]:
+            if request.POST.get(key) and request.POST.get(key) != \
+               CURRENT_VALUES[int(key)]:
                 try:
-                    String.objects.filter(translator=translator,
-                                     id=int(key),
-                                     frozen=False).update(text=request.POST.get(key))
+                    String.objects.filter(
+                        translator=translator,
+                        id=int(key),
+                        frozen=False
+                    ).update(
+                        text=request.POST.get(key)
+                    )
                 except String.DoesNotExist:
                     return HttpResponse('There was an error!')
             else:
@@ -162,8 +172,11 @@ def ModifyStringsHandler(request):
         CURRENT_VALUES = {}
         return HttpResponseRedirect('/modify/')
     else:
-        strings_list = String.objects.filter(translator=translator, frozen=False)
-        paginator = Paginator(strings_list, 2) # Show 15 strings per page
+        strings_list = String.objects.filter(
+            translator=translator,
+            frozen=False
+        )
+        paginator = Paginator(strings_list, 2)  # Show 15 strings per page
         page = request.GET.get('page')
         try:
             strings = paginator.page(page)
@@ -177,13 +190,15 @@ def ModifyStringsHandler(request):
         return render_to_response('modify.html', context,
                                   context_instance=RequestContext(request))
 
+
 @staff_member_required
 def GenerateHandler(request):
-    if request.method=='POST':
+    if request.method == 'POST':
         form = GenerateForm(request.POST)
         if form.is_valid():
-
-            return HttpResponse(form.cleaned_data['language'])
+            language = form.cleaned_data['language']
+            app = form.cleaned_data['app']
+            return generate.Android().download_files(language, app)
         else:
             return render_to_response('login.html', {'form': form},
                                       context_instance=RequestContext(request))
--- localization/utils/generate.py
+++ localization/utils/generate.py
@@ -1,35 +1,54 @@
+# -*- coding: utf-8 -*-
+
 from app.models import String
 from django.http import HttpResponse
+from django.core.servers.basehttp import FileWrapper
 from zipfile import ZipFile
 from StringIO import StringIO
-import elementtree.ElementTree as ET
+from lxml import etree
+
 
 class Android():
-    def __init__(self, app, langs):
-        app = app
-        langs = langs
-    def generate_file(self, lang):
-        if lang.name=='English':
-            strings = String.objects.filter(enabled=True, language=lang,
-                                            app = app)
-        else:
-            strings = String.object.filter(enabled=True, language=lang,
-                                            original_string__app = app)
 
-    def download_files(self):
+    def generate_file(self, lang, app):
+        if lang.name == 'English':
+            strings = String.objects.filter(enabled=True, language=lang,
+                                            app=app)
+        else:
+            strings = String.objects.filter(enabled=True, language=lang,
+                                            original_string__app=app)
+        root = etree.Element('resources')
+        for string in strings:
+            line = etree.SubElement(root, "string",
+                                    name=string.android_name_string)
+            line.text = string.text
+        content = etree.tostring(root, encoding='utf-8',
+                                 xml_declaration=True, pretty_print=True)
+        return content
+
+    def download_files(self, langs, app):
         in_memory = StringIO()
 
-        zip_file = ZipFile(in_memory, 'w')
-        for l in self.langs:
+        zip_file = ZipFile(in_memory, 'a')
+        for l in langs:
             if l.name == 'English':
-                zip_file.writestr('values/strings.xml', generate_file(l))
+                zip_file.writestr('values/strings.xml',
+                                  self.generate_file(l, app))
             else:
                 zip_file.writestr('values-' + l.iso_639 + '/strings.xml',
-                                  generate_file(l))
-        for file in filelist:
+                                  self.generate_file(l, app))
+
+        # fix for Linux zip files read in Windows
+        for file in zip_file.filelist:
             file.create_system = 0
 
         zip_file.close()
-        compressed = HttpResponse(mimetype='application/zip')
-        compressed['Content-Disposition'] = 'attachment; filename='+app.__unicode__()
-        return compressed
+
+        response = HttpResponse(mimetype='application/zip')
+        response['Content-Disposition'] = 'attachment; filename=%s.zip' \
+                                          % (app.__unicode__().replace(' ', '-'))
+        response['Content-Length'] = in_memory.tell
+
+        in_memory.seek(0)
+        response.write(in_memory.read())
+        return response
